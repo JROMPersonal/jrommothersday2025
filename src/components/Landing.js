3f1c2c206e311be81a95c03093866946d9febcc0
James Meyer <jamesmeyer@macbookpro.mynetworksettings.com>
2025-05-11 17:15:40 -0700

    Structure: Adding components directory
    Added the "Components" directory and some JS Files
    to handle different pages within the webapp

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <h1>Happy Mother's Day, Mom!</h1>
+      <p>Here’s a little something to celebrate you and all the things you love!</p>
+    </div>
+  );
+}
+
+export default Landing;
