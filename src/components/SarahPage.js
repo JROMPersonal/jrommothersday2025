865dab74da8ffb5f25c660f1c2bd30a008872f87
JROMPersonal <128358203+JROMPersonal@users.noreply.github.com>
2025-05-11 18:13:52 -0700

    Update SarahPage.js

--- src/components/SarahPage.js
+++ src/components/SarahPage.js
@@ -15,7 +15,7 @@ const SarahPage = () => {
       >
       <h2>Happy Mother's Day, Sarah!</h2>
       <p>Hope you've had a great day! Love James</p>
-      <img src={`${process.env.PUBLIC_URL}/photos/Sar1.JPG`} alt="Sar1" className="sister-image" />
+      <img src={`${process.env.PUBLIC_URL}/photos/Sar1.JPG`} alt="Sar1.jpg" className="sister-image" />
       <img src={`${process.env.PUBLIC_URL}/photos/Sisters2.JPG`} alt="Sisters2" className="sister-image" />
       <button className="back-button" onClick={() => navigate('/')}>← Back To Home</button>
 
